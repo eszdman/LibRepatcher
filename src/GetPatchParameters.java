3e876980c29b9a69c2af65bfd78d04ded2219606
Eszdman <eszdman2015@gmail.com>
2020-10-23 00:09:23 +0300

    Added 16 radix support

--- src/GetPatchParameters.java
+++ src/GetPatchParameters.java
@@ -4,6 +4,11 @@ import java.io.IOException;
 import java.util.Properties;
 
 public class GetPatchParameters {
+    private static int readint(String in){
+        if(in.contains("0x")){
+            return Integer.parseInt(in.replace("0x",""),16);
+        } else return Integer.parseInt(in);
+    }
 public static void ReadPatchParams(Interface inter, File inputPatch){
     Properties props = new Properties();
     try {
@@ -16,10 +21,10 @@ public static void ReadPatchParams(Interface inter, File inputPatch){
         String temp;
         temp=props.getProperty("ShiftIndex");
         if(temp != null)
-        inter.shift = Integer.parseInt(temp);
+        inter.shift = readint(temp);
         temp=props.getProperty("StartIndex");
         if(temp != null)
-            inter.start_index = Integer.parseInt(temp);
+            inter.start_index = readint(temp);
         temp=props.getProperty("EveryPattern");
         if(temp != null)
             inter.every_pattern = Boolean.parseBoolean(temp);
@@ -28,7 +33,7 @@ public static void ReadPatchParams(Interface inter, File inputPatch){
         inter.find_percentage = Double.parseDouble(temp);
         temp=props.getProperty("SkipPattern");
         if(temp != null)
-        inter.skip_pattern = Integer.parseInt(temp);
+        inter.skip_pattern = readint(temp);
 
 }
 }
